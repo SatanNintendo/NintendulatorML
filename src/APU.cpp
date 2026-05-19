488862d9ba13b1106e6202386db05e345b566912
Sergey <satan.death@mail.ru>
2026-05-19 23:21:01 +0400

    Replace error messages with localized strings

--- src/APU.cpp
+++ src/APU.cpp
@@ -1020,7 +1020,7 @@ void	Init (void)
 	if (FAILED(DirectSoundCreate(&DSDEVID_DefaultPlayback, &DirectSound, NULL)))
 	{
 		Destroy();
-		MessageBox(hMainWnd, _T("Failed to initialize DirectSound!"), _T("Nintendulator"), MB_OK);
+		MessageBox(hMainWnd, Lang::GetString(LANG_ERR_APU_INIT), Lang::GetString(LANG_DLG_NINTENDULATOR), MB_OK);
 		return;
 	}
 	if (FAILED(DirectSound->SetCooperativeLevel(hMainWnd, DSSCL_PRIORITY)))
@@ -1060,7 +1060,7 @@ void	Start (void)
 	if (FAILED(DirectSound->CreateSoundBuffer(&DSBD, &PrimaryBuffer, NULL)))
 	{
 		Stop();
-		MessageBox(hMainWnd, _T("Sound buffer error!"), _T("Nintendulator"), MB_OK);
+		MessageBox(hMainWnd, Lang::GetString(LANG_ERR_APU_BUFFER2), Lang::GetString(LANG_DLG_NINTENDULATOR), MB_OK);
 		return;
 	}
 
@@ -1074,7 +1074,7 @@ void	Start (void)
 	if (FAILED(PrimaryBuffer->SetFormat(&WFX)))
 	{
 		Stop();
-		MessageBox(hMainWnd, _T("Sound format error!"), _T("Nintendulator"), MB_OK);
+		MessageBox(hMainWnd, Lang::GetString(LANG_ERR_APU_FORMAT2), Lang::GetString(LANG_DLG_NINTENDULATOR), MB_OK);
 		return;
 	}
 	if (FAILED(PrimaryBuffer->Play(0, 0, DSBPLAY_LOOPING)))
@@ -1094,7 +1094,7 @@ void	Start (void)
 		MessageBox(hMainWnd, _T("Sound buffer error!"), _T("Nintendulator"), MB_OK);
 		return;
 	}
-	EI.DbgOut(_T("APU disabled."));
+	EI.DbgOut(Lang::GetString(LANG_MSG_APU_DISABLED2));
 #endif	/* !NSFPLAYER */
 }
 
