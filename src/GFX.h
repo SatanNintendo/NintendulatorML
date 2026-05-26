bf46f80c14db1905fc39a4e0c6d65d6d1a94c112
Sergey <satan.death@mail.ru>
2026-05-26 21:31:35 +0400

    Update GFX.h

--- src/GFX.h
+++ src/GFX.h
@@ -16,7 +16,7 @@ enum PALETTE { PALETTE_NTSC, PALETTE_PAL, PALETTE_PC10, PALETTE_VS1, PALETTE_VS2
 
 extern unsigned char RawPalette[8][64][3];
 extern unsigned long Palette32[512];
-extern BOOL Fullscreen, Scanlines, Bilinear, IntegerScale;
+extern BOOL Fullscreen, Scanlines, Bilinear, IntegerScale, MatchMonitorRate;
 
 // Отступы и множитель для Integer Scaling
 extern int ISBorderX, ISBorderY, ISMult;
