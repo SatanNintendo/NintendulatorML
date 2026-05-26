8998738429fcef0859de4bbf8a3be5b3a684a292
Sergey <satan.death@mail.ru>
2026-05-27 00:41:05 +0400

    Update GFX.cpp

--- src/GFX.cpp
+++ src/GFX.cpp
@@ -122,9 +122,18 @@ BOOL UseOpenGL(void)
 static BOOL GL_Init(int winW, int winH)
 {
 	hGLDC = GetDC(hMainWnd);
-	if (!hGLDC) return FALSE;
+	if (!hGLDC)
+		return FALSE;
 
-	PIXELFORMATDESCRIPTOR pfd = {};
+	PIXELFORMATDESCRIPTOR pfd;
+	ZeroMemory(&pfd, sizeof(pfd));
+
+	pfd.nSize = sizeof(pfd);
+	pfd.nVersion = 1;
+	pfd.dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
+	pfd.iPixelType = PFD_TYPE_RGBA;
+	pfd.cColorBits = 32;
+	pfd.iLayerType = PFD_MAIN_PLANE;
 	pfd.nSize      = sizeof(pfd);
 	pfd.nVersion   = 1;
 	pfd.dwFlags    = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
