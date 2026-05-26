34b75b7f5faa514fc50a3e435b880ce047694b0a
Sergey <satan.death@mail.ru>
2026-05-26 23:24:18 +0400

    Update GFX.cpp

--- src/GFX.cpp
+++ src/GFX.cpp
@@ -153,8 +153,15 @@ static BOOL GL_Init(int winW, int winH)
 	glGenTextures(1, &glTex);
 	glBindTexture(GL_TEXTURE_2D, glTex);
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, 256, 240, 0, GL_BGRA_EXT, GL_UNSIGNED_BYTE, NULL);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(
+	GL_TEXTURE_2D,
+	GL_TEXTURE_MIN_FILTER,
+	Bilinear ? GL_LINEAR : GL_NEAREST);
+
+glTexParameteri(
+	GL_TEXTURE_2D,
+	GL_TEXTURE_MAG_FILTER,
+	Bilinear ? GL_LINEAR : GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 	glEnable(GL_TEXTURE_2D);
