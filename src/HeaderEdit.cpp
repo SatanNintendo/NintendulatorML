c8f38368178ca054544416e12802183654a50509
Sergey <satan.death@mail.ru>
2026-05-31 21:43:45 +0400

    Update HeaderEdit.cpp

--- src/HeaderEdit.cpp
+++ src/HeaderEdit.cpp
@@ -242,7 +242,7 @@ bool	OpenROM (void)
 		memset(header+7, 0, 9);
 	}
 	else if (CheckHeader(false) && 
-		(MessageBox(hMainWnd, Lang::GetString(LANG_ERR_HEADER_INCONSISTENT), Lang::GetString(LANG_DLG_NINTENDULATOR), MB_YESNO | MB_ICONQUESTION) == IDYES)
+		(MessageBox(hMainWnd, Lang::GetString(LANG_ERR_HEADER_INCONSISTENT), Lang::GetString(LANG_DLG_NINTENDULATOR), MB_YESNO | MB_ICONQUESTION) == IDYES))
 	{
 		CheckHeader(true);
 	}
